7962acd805d7ff6adf3e3d687b6dcf86a729d80b
Angelica <angiepanda1412@gmail.com>
2025-02-19 20:43:11 -0600

    objetos

---
+++ Objetos/poo.js
@@ -0,0 +1,60 @@
+let edad  = "una edad"
+let nacionalidad = "una nacionalidad"
+//paso 1
+class Persona {
+
+    constructor(nombre, edad, nacionalidad) {
+        this.nombre = nombre
+        this.edad = edad
+        this.nacionalidad = nacionalidad
+    }
+
+    saludar(){
+        console.log("Hola, mi nombre es", this.nombre, "tengo", this.edad, "años, soy", this.nacionalidad);
+        
+    }
+
+    despedir(nombreAmigo){
+        console.log("Hasta luego,", nombreAmigo);
+        
+    }
+}
+
+//paso 2
+let persona1 = new Persona("Pedro", 23, "Mexicano")
+
+console.log(persona1)
+//paso3
+persona1.saludar()
+persona1.despedir("Fernando")
+
+//Ejercicio 1
+let persona2 = new Persona("Lucía", 30, "Canadiense")
+
+//Ejercicio 2: Lucía se despide de Mario
+persona2.despedir("Mario")
+
+console.log("----------------------------------")
+// Definición del objeto libro
+const libro = {
+    titulo: "Fahrenheit 451",
+    autor: "Ray Bradbury",
+    año: 1953,
+    estado: "disponible",
+    capitulos: [],
+
+        describirLibro: function () {
+            console.log("Libro titulado ",this.titulo, "escrito por" , this.autor , "en el año", this.año, "el estado es:",this.estado);
+        },
+
+        agregarCapitulo: function (nombreCapitulo) {
+            this.capitulos.push(nombreCapitulo);
+        console.log("Capítulo ", nombreCapitulo ," agregado al libro ", this.titulo);
+    },
+
+};
+
+  libro.describirLibro(); // Muestra la descripción del libro
+  libro.agregarCapitulo("Capítulo 1: Introducción"); // Agrega un capítulo
+  libro.agregarCapitulo("Capítulo 2: Desarrollo"); // Agrega otro capítulo
+  libro.describirLibro(); // Muestra la descripción actualizada
